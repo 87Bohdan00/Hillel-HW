07148eb9e70a574c4b98bc42ecd3c43adf4b03be
Bohdan <theyoung254@gmail.com>
2023-02-23 13:13:36 +0200

    first commit

---
+++ Hillel FE/hw1_hello_user/script.js
@@ -0,0 +1,9 @@
+alert('Hello!');
+
+const name = prompt('What is your name?');
+const bday = prompt('What is your birth year?');
+const country = prompt('Where are you from?');
+
+const age = 2023 - bday;
+
+alert('Nice to meet you, ' + name + '! You are ' + age + ' years old! You are from ' + country + '!');
---
+++ Hillel FE/hw2_calculator/script.js
@@ -0,0 +1,13 @@
+const a = Number(prompt('Enter the first number:'));
+const b = Number(prompt('Enter the second number:'));
+
+const sum = a + b;
+const diff = a - b;
+const mult = a * b;
+const div = a / b;
+
+console.log('Calculations are finished!');
+console.log('Sum: ' + a + ' + ' + b + ' = ' + sum);
+console.log('Diff: ' + a + ' - ' + b + ' = ' + diff);
+console.log('Mult: ' + a + ' * ' + b + ' = ' + mult);
+console.log('Div: ' + a + ' / ' + b + ' = ' + div);
